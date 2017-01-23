3e690d74ce1f0917d8464cecf2e3bc160f6686f2
Qs <quan.s.box@gmail.com>
2017-01-22 20:17:49 -0500

    tune processImg()

--- util_vision.py
+++ util_vision.py
@@ -1,6 +1,16 @@
 import numpy as np
 import cv2
 
+
+def extractSatuCh(img):
+
+    img_hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+    # 2) Apply a threshold to the S channel
+    ch_satu = img_hls[:,:,2]
+
+    return ch_satu
+
+
 # a function that applies Sobel x or y, 
 # then takes an absolute value and applies a threshold.
 # Note: calling your function with orient='x', thresh_min=5, thresh_max=100
@@ -36,11 +46,11 @@ def abs_sobel_thresh(img, orient='x', thresh_min=0, thresh_max=255):
 # a function that applies Sobel x and y, 
 # then computes the magnitude of the gradient
 # and applies a threshold
-def mag_thresh(img, sobel_kernel=3, mag_thresh=(0, 255)):
+def mag_thresh(image, sobel_kernel=3, thresh=(0, 255)):
     
     # 1) Convert to grayscale
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-
+    # img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    img = extractSatuCh(image)
     # 2) Take the derivative in x or y given orient = 'x' or 'y'
     
     sobel_x = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
@@ -58,8 +68,8 @@ def mag_thresh(img, sobel_kernel=3, mag_thresh=(0, 255)):
             # is > thresh_min and < thresh_max
     sobel_mask = np.zeros_like(sobel_norm)
     
-    thresh_min = mag_thresh[0]
-    thresh_max = mag_thresh[1]
+    thresh_min = thresh[0]
+    thresh_max = thresh[1]
     sobel_mask[(sobel_norm>=thresh_min) & (sobel_norm<=thresh_max)] = 1
     
     # 6) Return this mask as your binary_output image
@@ -67,19 +77,21 @@ def mag_thresh(img, sobel_kernel=3, mag_thresh=(0, 255)):
     
     return binary_output
 
+
 # a function that applies Sobel x and y, 
 # then computes the direction of the gradient
 # and applies a threshold.
-def dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi/2)):
+def dir_threshold(image, sobel_kernel=3, thresh=(0, np.pi/2)):
     
-    # Apply the following steps to img
+    # Apply the following steps to image
     # 1) Convert to grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    img = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    # img = extractSatuCh(image)
 
     # 2) Take the gradient in x and y separately
-    sobel_x = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobel_x = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
 
-    sobel_y = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)    
+    sobel_y = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=sobel_kernel)    
     # 3) Take the absolute value of the x and y gradients
     sobel_x_abs = np.absolute(sobel_x)
     sobel_y_abs = np.absolute(sobel_y)
@@ -115,30 +127,54 @@ def hls_select(img, thresh=(0, 255)):
 
     return binary_output
 
+# a function that thresholds the S-channel of HLS
+# Use exclusive lower bound (>) and inclusive upper (<=)
+def hls_select_s(img, thresh=(0, 255)):
+    # 1) Convert to HLS color space
+    img_hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+    # 2) Apply a threshold to the S channel
+    ch_hue = img_hls[:,:,0]
+    # ch_satu = img_hls[:,:,2]
+    img_1ch = ch_hue
+    binary_ch = np.zeros_like(img_1ch)
+    thre_min = thresh[0]
+    thre_max = thresh[1]
+    binary_ch[ (img_1ch>thre_min) & (img_1ch<=thre_max)] =1
+    # 3) Return a binary image of threshold result
+    binary_output = binary_ch
+    
+
+    return binary_output
 
 class qVision:
     def __init__(self):
         pass
 
-    def processImg(self, img_rgb):
+    def processImg(self, img_rgb, debug=False):
 
-        image = np.copy(img_rgb)
-        # image =  img_rgb
+        # image = np.copy(img_rgb)
+        image_orig = np.copy(img_rgb)
 
-        hls_binary = hls_select(img_rgb, thresh=(100, 255))
-        image[(hls_binary != 1)]  = 0
+
+        hls_binary = hls_select(image_orig, thresh=(66, 255))
+        # image[(hls_binary != 1)]  = 0
         # Run the function
-        mag_binary = mag_thresh(image, sobel_kernel=9, mag_thresh=(33, 150))
-        dir_binary = dir_threshold(image, sobel_kernel=9, thresh=(np.pi*20.0/180.0, np.pi*80.0/180.0)) 
+        mag_binary = mag_thresh(image_orig, sobel_kernel=3, thresh=(50, 120 ))
+
+        dir_binary = dir_threshold(image_orig, sobel_kernel=15, thresh=(np.pi*20.0/180.0, np.pi*80.0/180.0)) 
+        # dir_binary = dir_threshold(image_orig2, sobel_kernel=15, thresh=(0.7, 1.2) ) # 40~80 degree
         # dir_binary = dir_threshold(mag_binary, sobel_kernel=11, thresh=(0.7, 1.3), enable_binary_map=True) 
 
 
         combined = np.zeros_like(dir_binary)
         # combined[(hls_binary == 1) ] = 1
-        combined[  (dir_binary == 1) & (mag_binary == 1)   ] = 1
-        # combined[ (mag_binary == 1)   ] = 1
+        # combined[  (dir_binary == 1) & (mag_binary == 1)   ] = 1
+        combined[ (dir_binary == 1) & (mag_binary == 1) & (hls_binary == 1) ] = 1
         # Plot the result
 
+        if debug == True:
+            DBG_CompareThreeGrayImages(hls_binary , mag_binary,  dir_binary ,'HLS satu', 'mag', 'directional' )
+        
 
         img_final = combined
         return img_final
@@ -162,6 +198,27 @@ def DBG_CompareImages(img1, img2, title1, title2, cmap2=None, save_to_file=''):
     plt.show()
 
 
+def DBG_CompareThreeGrayImages(img1, img2, img3, title1, title2, title3 ):
+    import matplotlib.pyplot as plt
+    import matplotlib.image as mpimg
+
+    f, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(21, 6))
+    f.tight_layout()
+
+    ax1.imshow(img1, cmap='gray', vmin = 0, vmax = 1)
+    ax1.set_title(title1, fontsize=30)
+
+    ax2.imshow(img2, cmap='gray', vmin = 0, vmax = 1)
+    ax2.set_title(title2, fontsize=30)
+
+    ax3.imshow(img3, cmap='gray', vmin = 0, vmax = 1)
+    ax3.set_title(title3, fontsize=30)
+
+    plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+
+    plt.show()
+
+
 def main():
 
     vision = qVision()
@@ -175,7 +232,6 @@ def main():
     for img_loc in images_loc:
         img_bgr = cv2.imread(img_loc)
 
-
         img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
         img_procd = vision.processImg(img_rgb)
 
@@ -187,7 +243,7 @@ def main():
 
 
 
-    img_bgr = cv2.imread('udacity/test_images/test2.jpg')
+    img_bgr = cv2.imread('udacity/test_images/test5.jpg')
 
 
     img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
